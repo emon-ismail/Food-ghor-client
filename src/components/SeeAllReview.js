923cb8f238ecd5fe01d8ad7d936c0834e7a211c5
Tonmoy-Roy <emonismail69@gmail.com>
2022-11-10 18:12:32 +0600

    fixed

--- src/components/SeeAllReview.js
+++ src/components/SeeAllReview.js
@@ -3,7 +3,13 @@ import React from 'react';
 const SeeAllReview = () => {
     return (
         <div>
-            <h1>see all review</h1>
+           <div className="mockup-window border bg-base-300">
+  <div className="flex justify-center px-4 py-16 bg-base-200"><p>
+  Need more choices, and in the morning should open before 7.30. It needs to either have a buffet service or faster serving time. It also needs to reduce mistakes in taking orders
+    
+    </p>
+    </div>
+</div>
         </div>
     );
 };
